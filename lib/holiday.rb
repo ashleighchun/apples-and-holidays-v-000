02039f56152bdc0dd28d80e02b24c790418623bc
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-06 17:14:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_supplies.each do |season, holidays|
-    holidayhash.each do |attribute, data|
+    holiday_hash.each do |attribute, data|
       if data.include?("BBQ")
         puts "#{attribute}"
       end
